64323397d64e33677416a7c1d8365c2200c6682e
danielschwartz3 <71156919+danielschwartz3@users.noreply.github.com>
2020-09-13 04:38:35 -0400

    Cleaning up the code
    removing unused sections of code (chat_rooms.py and parts of app.py)

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from flask_socketio import SocketIO, join_room
 from login_required import login_required
 #***********
 from server.firestoreWrapper import FirestoreCollections
-# from server.models.chat_rooms import chat_room_page
 from server.models.feed import feed_page
 from server.models.genres import genre_page
 from server.models.performances import performance_page
@@ -25,7 +24,6 @@ app.secret_key = "^d*ÎˇÁÔÒÔ˝◊ÇÎNJGHCFDDRT^YUOIÔ˝ÇÎ˛�
 app.register_blueprint(genre_page)
 app.register_blueprint(feed_page)
 app.register_blueprint(user_page)
-# app.register_blueprint(chat_room_page)
 app.register_blueprint(performance_page)
 app.register_blueprint(scheduled_performance_page)
 # Use a service account
@@ -35,9 +33,6 @@ app.register_blueprint(scheduled_performance_page)
 def main():
     return "Welcome to encore"
 
-
-
-# chat_room_page = Blueprint('chat_room_page', __name__)
 
 @app.route("/chat_rooms", methods=['GET'])
 def get_all_chat_rooms():
@@ -71,66 +66,3 @@ def handle_send_message_event(data):
                                                                             data['chat_room_id'], 
                                                                             data['message']))
         socketio.emit('receive_message', data, chat_room_id = data['chat_room_id'])
-
-
-# @chat_room_page.route("/chat_rooms/<chat_room_id>/receive", methods=['GET'])
-# def receive_message():
-#     pass
-
-# @chat_room_page.route("/chat_rooms/<chat_room_id>/send", methods=['POST'])
-# @login_required
-# def send_message():
-#     chat_room_id = request.form['chat_room_id']
-#     user_id = request.form['user_id']
-#     message_text = request.form['message_text']
-#     message_datetime = request.form['datetime']
-#     # message = Message(message_text,message_datetime)
-#     #TODO post messages to server
-#     # chat_room = FirestoreCollections.chat_rooms_ref().document(chat_room_id)
-
-
-# @chat_room_page.route('/chat_rooms/join', methods=['POST'])
-# def join_chat_room():
-#     chat_room_id = request.form['chat_room_id']
-#     user_id = request.form['user_id']
-#     chat_room = FirestoreCollections.chat_rooms_ref().document(chat_room_id)
-#     members = chat_room.data['members'].append(user_id)
-#     chat_room.set({'members': members})
-#     #TODO connect sockets
-
-# @chat_room_page.route('/chat_rooms/join', methods=['POST'])
-# def leave_chat_room():
-#     pass
-
-# @login_required
-# @chat_room_page.route('/chat/new', methods=['POST'])
-# def create_chat_room():
-#     pass
-
-# @login_required
-# @chat_room_page.route('/chat/stop', methods=['POST'])
-# def stop_chat_room():
-#     pass
-
-class ChatRoom:
-    def __init__(self, chat_room_id, is_active):
-        self.chat_room_id = chat_room_id
-        self.members = []
-        self.is_active = is_active
-
-    def to_dict(self):
-        return {'chat_room_id': self.chat_room_id,
-                'members': self.members,
-                'is_active': self.is_active}
-
-    def store(self):
-        FirestoreCollections.chat_rooms_ref().add(self.to_dict())
-
-    class Message:
-        def __init__(self, text, datetime):
-            self.text = text
-            self.datetime = datetime
-        
-        def to_dict(self):
-            return {'text': self.text,
-                    'datetime': self.datetime}
